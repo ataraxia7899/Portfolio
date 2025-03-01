9944a551d8d73a115f318c9e82c1c51bb1ed8b26
ataraxia7899 <ataraxia7899@gmail.com>
2025-03-02 00:32:59 +0900

    커버영역 스타일링 비율 수정

--- src/components/Cover.jsx
+++ src/components/Cover.jsx
@@ -1,14 +1,18 @@
+import '../App.css';
+
 export default function Cover() {
 	const CoverStyle = {
-		width: '142.9%',
+		width: '100vw', // 뷰포트 전체 너비로 설정
 		height: '14rem',
 		backgroundColor: '#ffefe3',
-		position: 'absolute',
-		// zIndex: '-1',
-		// left: '0', // 왼쪽 여백 제거
-		// right: '0', // 오른쪽 여백 제거
-		marginLeft: '-50vw', // 부모 컨테이너의 margin 무시
-		marginRight: '-50vw', // 부모 컨테이너의 margin 무시
+		position: 'absolute', // 절대 위치 설정
+		left: '50%', // 요소를 수평으로 50% 위치에 배치
+		transform: 'translateX(-50%)', // 정확한 중앙 정렬을 위해 요소를 왼쪽으로 50% 이동
+		marginLeft: '0', // 왼쪽 여백 제거
+		marginRight: '0', // 오른쪽 여백 제거
+		top: '0', // 맨위로
+		maxWidth: '100%', // 최대 너비 제한
+		boxSizing: 'border-box', // 패딩과 보더를 너비에 포함
 	};
 
 	return <div style={CoverStyle} />;
