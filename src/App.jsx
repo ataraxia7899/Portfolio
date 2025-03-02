5161ba2f6cad6f2b2edfe52fb2c71337c7dc7f18
ataraxia7899 <ataraxia7899@gmail.com>
2025-03-02 12:58:05 +0900

    모바일 환경 배경 스타일 수정 2

--- src/App.jsx
+++ src/App.jsx
@@ -19,28 +19,33 @@ export default function App() {
 		minWidth: '320px', // 최소 너비
 	};
 
+	const contentStyle = {
+		width: '80%', // 더 나은 균형감을 위해 조정된 너비
+		maxWidth: '1200px',
+		background: '#333333',
+		position: 'relative',
+		top: '10px',
+		padding: '0 40px 25px',
+		margin: '0 auto', // div를 수평으로 중앙 정렬
+		boxSizing: 'border-box', // 너비 계산에 패딩 포함
+		overflowX: 'hidden', // 가로 스크롤 방지
+		'@media screen and (max-width: 768px)': {
+			width: '100%',
+			maxWidth: 'none',
+			margin: '0',
+			padding: '20px',
+			position: 'relative',
+			top: '-215px', // 커버 높이만큼 위로 이동
+			minHeight: '100vh', // 전체 화면 높이
+			borderRadius: '25px 25px 0 0', // 상단 모서리만 둥글게
+		},
+	};
+
 	return (
 		<>
 			<div style={Avoid_Cover_Style}>
 				{/* 맨위에 커버를 설치해서 겹치지 않도록 위치 조절 */}
-				<div
-					style={{
-						width: '80%', // 더 나은 균형감을 위해 조정된 너비
-						maxWidth: '1200px',
-						background: '#333333',
-						position: 'relative',
-						top: '10px',
-						padding: '0 40px 25px',
-						margin: '0 auto', // div를 수평으로 중앙 정렬
-						boxSizing: 'border-box', // 너비 계산에 패딩 포함
-						overflowX: 'hidden', // 가로 스크롤 방지
-						['@media (max-width: 768px)']: {
-							// 모바일 환경
-							width: '100%',
-							padding: '0 10px 25px',
-						},
-					}}
-				>
+				<div style={contentStyle}>
 					<Title />
 					<Basic_explanation />
 					<Educations />
